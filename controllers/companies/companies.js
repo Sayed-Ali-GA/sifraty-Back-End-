555f466e2c0be1351c3cd055a2b1f086fa95df9f
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-11-02 22:42:04 +0300

    Add folders

--- controllers/companies/companies.js
+++ controllers/companies/companies.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const pool = require('../config/db');
-const { authenticateToken } = require('../middleware/verify-token');
+const pool = require('../../config/db');
+const { authenticateToken } = require('../../middleware/verify-token');
 
 router.use(authenticateToken);
 
